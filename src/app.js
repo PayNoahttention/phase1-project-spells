ec6da60a773ae199ef5b5ec7d05bc46a601d04c5
Noah Solis <NSolis1993@gmail.com>
2022-09-08 17:51:40 -0500

    Added search functionality but the CSS of the images aludes me...

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
-// TODO Move scale from css to onhover in js
+// TODO: Submit button to send spell info to card
+// Spells URL
+// const spellsUrl = 'https://fedeperin-harry-potter-api-en.herokuapp.com/spells'
 
 // setting up variables of house images
 const houseGryffindor = document.getElementById('gryffindor');
 const houseHufflepuff = document.getElementById('hufflepuff');
 const houseRavenclaw = document.getElementById('ravenclaw');
 const houseSlytherin = document.getElementById('slytherin');
+
+// Setting up variables for search
+
+const spellSearch = document.getElementById('spell-search');
+const spellList = document.getElementById('spell-list');
 
 
 // changing background color and font color to house colors on click 
@@ -13,10 +20,11 @@ const houseSlytherin = document.getElementById('slytherin');
 houseGryffindor.addEventListener('click', (e) => document.body.style.backgroundColor = "#740001");
 houseGryffindor.addEventListener('click', (e) => document.body.style.color = "#D3A625");
 
+
+
 // Hufflepuff
 houseHufflepuff.addEventListener('click', (e) => document.body.style.backgroundColor = "#FFD800");
 houseHufflepuff.addEventListener('click', (e) => document.body.style.color = "#000000");
-houseHufflepuff.addEventListener('click', (e) => document.body.style.removeProperty = "text-shadow"); // to stop text from chonky look
 
 // Ravenclaw
 houseRavenclaw.addEventListener('click', (e) => document.body.style.backgroundColor = "#0E1A40");
@@ -25,4 +33,40 @@ houseRavenclaw.addEventListener('click', (e) => document.body.style.color = "#94
 // Slytherin
 houseSlytherin.addEventListener('click', (e) => document.body.style.backgroundColor = "#1A472A");
 houseSlytherin.addEventListener('click', (e) => document.body.style.color = "#5D5D5D");
+
+// Search Spells and Filters Results
+
+const searchSpells = async (searchText) => {
+    const res = await fetch('https://fedeperin-harry-potter-api-en.herokuapp.com/spells');
+    const arrSpells = await res.json();
+
+    let matches = arrSpells.filter((spellName) => {
+        const regex = new RegExp(`^${searchText}`, 'gi')
+        return spellName.spell.match(regex);
+    });
+
+    if (searchText.length === 0) {
+        matches = [];
+        spellList.innerHTML = ''
+    }
+    
+    outputSpellsInfo(matches);
+};
+
+// Send Results to HTML
+
+const outputSpellsInfo = matches => {
+    if (matches.length > 0) {
+        const html = matches.map(match => `
+        <div id="spellResult">
+            <h4 class="spellName">${match.spell} <!-- <span>Use: ${match.use}</span>--></h4>
+        </div>`)
+        .join('');
+
+        spellList.innerHTML = html;
+    }
+};
+
+
+spellSearch.addEventListener('input', () => searchSpells(spellSearch.value));
 
